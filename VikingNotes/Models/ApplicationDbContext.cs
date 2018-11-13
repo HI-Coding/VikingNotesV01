caff11154f1545c122a8752b5dfe27f9a9702c35
IHCoding <s.imal@yahoo.com>
2018-11-13 11:47:00 +0100

    Add project files.

---
+++ VikingNotes/Models/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace VikingNotes.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Quiz> Guizzes { get; set; } // step 3c
+        public DbSet<Genre> Genres { get; set; } // step 4
+        public DbSet<Attendance> Attendances { get; set; } 
+
+
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a =>a.Quiz)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
